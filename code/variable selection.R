1f96825f2355216a54fcd36998eeb7a8e276632e
danielye1999 <115088089+danielye1999@users.noreply.github.com>
2023-10-15 20:22:47 -0500

    Update variable selection.R, wrong file was uploaded previously

--- code/variable selection.R
+++ code/variable selection.R
@@ -1,8 +1,7 @@
+library(ggplot2)
+library(reshape2)
+library(car)
 
-# 1. Data Cleaning
-
-# Read the data
-# Load the dataset
 data <- read.csv("BodyFat.csv")
 
 # Remove rows with 0 or NA values
@@ -10,98 +9,51 @@ data <- data[rowSums(data == 0) == 0, ]
 data <- na.omit(data)
 data <- data[-which.max(data$BODYFAT), ]
 
-# 2. Variable Selection
-
 # Calculate the correlation matrix
-cor_matrix <- cor(data)
+correlation_matrix <- cor(data)
 
-# Extract the target variables based on our previous selection
-selected_vars <- c('ABDOMEN', 'ADIPOSITY', 'CHEST', 'BODYFAT')
+# Melting the correlation matrix for ggplot usability
+melted_correlation_matrix <- melt(correlation_matrix)
 
-# Subset the data
-data_selected <- data[, selected_vars]
+# Creating the heatmap
+ggplot(data = melted_correlation_matrix, aes(x = Var1, y = Var2, fill = value)) + 
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1),
+        axis.text.y = element_text(size = 12),
+        panel.grid.major = element_blank(), 
+        panel.border = element_blank(), 
+        axis.ticks = element_blank(), 
+        legend.position = c(1.4, 0), # Adjust this coordinate to move the legend
+        legend.direction = "horizontal") +
+  coord_fixed() +
+  labs(x = "", y = "", title = "Correlation Matrix Heatmap",
+       subtitle = "Color intensity and hue represent correlation strength")
 
-# 3. Model Training
 
-# Splitting the data into 80/20
-set.seed(42)
-train_indices <- sample(1:nrow(data_selected), 0.8 * nrow(data_selected))
-train_data <- data_selected[train_indices, ]
-test_data <- data_selected[-train_indices, ]
+# Extract correlations with 'BODYFAT'
+bodyfat_correlations <- correlation_matrix['BODYFAT', ]
 
-# PCR (Principal Component Regression)
-library(pls)
-pcr_model <- pcr(BODYFAT ~ ., data=train_data, validation="CV")
-pcr_pred <- predict(pcr_model, test_data[, -4])
+# Removing the 'BODYFAT' self-correlation
+bodyfat_correlations <- bodyfat_correlations[bodyfat_correlations != 1.0]
 
-# Random Forest
-library(randomForest)
-rf_model <- randomForest(BODYFAT ~ ., data=train_data)
-rf_pred <- predict(rf_model, test_data)
+# Sorting the correlations
+sorted_correlations <- sort(bodyfat_correlations, decreasing = TRUE)
 
-# Decision Tree
-library(rpart)
-tree_model <- rpart(BODYFAT ~ ., data=train_data)
-tree_pred <- predict(tree_model, test_data)
+# Printing the sorted correlations
+print(sorted_correlations)
 
-# Linear Regression
-lm_model <- lm(BODYFAT ~ ., data=train_data)
-lm_pred <- predict(lm_model, test_data)
+# Fit a linear model with the independent variables
+lm_model <- lm(BODYFAT ~ ABDOMEN+   ADIPOSITY    +   CHEST, data = data)
 
-# Lasso Regression
-library(glmnet)
-x_train <- as.matrix(train_data[, -4])
-y_train <- train_data$BODYFAT
-x_test <- as.matrix(test_data[, -4])
-lasso_model <- glmnet(x_train, y_train, alpha=1)
-lasso_pred <- predict(lasso_model, s=0.01, newx=x_test)
+# Calculate VIF
+vif_results <- vif(lm_model)
+print(vif_results)
 
-# Performance Metrics
-library(Metrics)
-models <- list(pcr=pcr_pred, rf=rf_pred, tree=tree_pred, lm=lm_pred, lasso=lasso_pred)
-
-#Interpretation:
-
-#The Decision Tree model has the best performance metrics across the board. It has the lowest MAE and MSE, meaning its predictions are, on average, the closest to the actual values and has the least squared error. Its R 
-#2 value is the highest, indicating it explains about 69.25% of the variance in the body fat percentage using the selected features.
-#The Random Forest model comes in as the second best, with slightly higher MAE and MSE values than the Decision Tree and an 
-#R2of 66.27%.PCR is performing poorly given its negative R
-#2 value.Given these results, the Decision Tree is the best model among those evaluated for this dataset using the selected features. It provides a balance of accuracy and interpretability, making it especially suitable for a "rule-of-thumb" approach.
-
-# Calculate R2 manually
-R2 <- function(y_true, y_pred) {
-  sst <- sum((y_true - mean(y_true))^2)
-  ssr <- sum((y_true - y_pred)^2)
-  return(1 - ssr/sst)
-}
-
-# Update results calculation
-results <- lapply(models, function(pred) {
-  list(
-    MAE = mae(test_data$BODYFAT, pred),
-    MSE = mse(test_data$BODYFAT, pred),
-    R2 = R2(test_data$BODYFAT, pred)
-  )
-})
-
-# Convert results list to a data frame
-results_df <- do.call(rbind, results)
-
-# Convert the list of lists into a data frame
-final_results <- data.frame(
-  Model = names(results),
-  MAE = sapply(results, function(x) x$MAE),
-  MSE = sapply(results, function(x) x$MSE),
-  R2 = sapply(results, function(x) x$R2)
-)
-
-# Print the final results table
-print(final_results)
-
-library(rpart.plot)
-
-# Visualize the Decision Tree without the extra argument
-rpart.plot(tree_model, type=4, main="Decision Tree for Estimating Body Fat Percentage")
 
 
 
